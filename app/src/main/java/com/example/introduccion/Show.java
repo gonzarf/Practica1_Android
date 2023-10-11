aff87865ed17882a510febf780ba31afd2ffda0b
gonzarf <gonzarf21@gmail.com>
2023-10-11 12:58:18 +0200

    Mostrar Datos BD

--- app/src/main/java/com/example/introduccion/Show.java
+++ app/src/main/java/com/example/introduccion/Show.java
@@ -2,7 +2,14 @@ package com.example.introduccion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.introduccion.Database.DatabaseAux;
 
 public class Show extends AppCompatActivity {
 
@@ -10,5 +17,31 @@ public class Show extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+    }
+
+    public void changetoMain(View view){
+        Intent nIntent = new Intent(Show.this, MainActivity.class);
+        startActivity(nIntent);
+    }
+
+    void showElements(){
+        SQLiteDatabase db = new DatabaseAux(this).getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM SoccerScout", null);
+        TextView nameShowView = findViewById(R.id.showName);
+        TextView emailShowView = findViewById(R.id.showEmail);
+
+        if(cursor.moveToFirst()){
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String email = cursor.getString(2);
+
+                nameShowView.setText(id + " " + name);
+                nameShowView.setText(email);
+            }while(cursor.moveToNext());
+        }
+
+        db.close();
     }
 }
